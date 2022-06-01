254a88e7b3f216a2e87eb4946d1d25dee835ae26
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-01 18:07:44 -0300

    [tc-hbtn_teste]add operacao basica

--- 0x00/calculadora/OperacaoBasicaTeste.java
+++ 0x00/calculadora/OperacaoBasicaTeste.java
@@ -1,4 +1,31 @@
-package calculadora;
+//package calculadora;
 
 public class OperacaoBasicaTeste {
+  OperacaoBasicaTeste() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static boolean testar_soma() {
+    double numb1 = 5;
+    double numb2 = 3;
+    return OperacaoBasica.somar(numb1, numb2) == 8;
+  }
+
+  public static boolean testar_subtracao() {
+    double numb1 = 5;
+    double numb2 = 3;
+    return OperacaoBasica.subtrair(numb1, numb2) == 2;
+  }
+
+  public static boolean testar_multiplicacao() {
+    double numb1 = 5;
+    double numb2 = 3;
+    return OperacaoBasica.multiplicar(numb1, numb2) == 15;
+  }
+
+  public static boolean testar_divisao() {
+    double numb1 = 9;
+    double numb2 = 3;
+    return OperacaoBasica.dividir(numb1, numb2) == 3;
+  }
 }
