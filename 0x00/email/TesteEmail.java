67dd6ff9092d97445391c5b1b1868c7b7c7ae31e
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-01 17:39:16 -0300

    [tc-hbtn_teste] add ternario

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -12,11 +12,11 @@ public class TesteEmail {
 
   public static boolean testar_email_sem_arroba() {
     String email = "email_testedominio.com.br";
-    return Pessoa.emailValid(email);
+    return !Pessoa.emailValid(email);
   }
 
   public static boolean testar_email_mais_50_caracteres() {
     String email = "email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br";
-    return Pessoa.emailValid(email);
+    return !Pessoa.emailValid(email);
   }
 }
