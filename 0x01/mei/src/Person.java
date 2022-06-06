63222c6252d27592e0126c9b7e4055da60ad8a68
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-06 15:44:05 -0300

    [junit I mei]impl mei class and test class

---
+++ 0x01/mei/src/Person.java
@@ -0,0 +1,94 @@
+
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Person {
+  private final String name;
+  private final String surname;
+  private final Date birthDate;
+
+  private boolean anotherCompanyOwner;
+  private boolean pensioner;
+  private boolean publicServer;
+
+  private float salary;
+
+  public Person(
+          String name,
+          String surname,
+          Date birthDate,
+          boolean anotherCompanyOwner,
+          boolean pensioner,
+          boolean publicServer) {
+    this.name = name;
+    this.surname = surname;
+    this.birthDate = birthDate;
+    this.anotherCompanyOwner = anotherCompanyOwner;
+    this.pensioner = pensioner;
+    this.publicServer = publicServer;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public float getSalary() {
+    return salary;
+  }
+
+  public void setSalary(float salary) {
+    this.salary = salary;
+  }
+
+  public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+    this.anotherCompanyOwner = anotherCompanyOwner;
+  }
+
+  public void setPensioner(boolean pensioner) {
+    this.pensioner = pensioner;
+  }
+
+  public void setPublicServer(boolean publicServer) {
+    this.publicServer = publicServer;
+  }
+
+  public String fullName() {
+    return String.format("%s %s", getName(), getSurname());
+  }
+
+  public float calculateYearlySalary() {
+    return this.salary * 12;
+  }
+
+  public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+    if ((birthDate != null) && (currentDate != null)) {
+      return Period.between(birthDate, currentDate).getYears();
+    } else {
+      return 0;
+    }
+  }
+
+  public boolean isMEI() {
+    LocalDate currentDate = LocalDate.now();
+    int age =
+            calculateAge(
+                    getBirthDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), currentDate);
+
+    if (getSalary() < 130000) return true;
+    if (age > 18) return true;
+    if (this.anotherCompanyOwner) return false;
+    if (this.pensioner) return false;
+    return !this.publicServer;
+  }
+}
---
+++ 0x01/mei/src/PersonTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+  private Person person;
+
+  void setup() {
+    person = new Person("Paul", "McCartney", new Date(2000), true, true, true);
+  }
+
+  @Before
+  public void before() throws Exception {
+    setup();
+  }
+
+  @Test
+  public void show_full_name() {
+    assertEquals("Paul McCartney", person.fullName());
+  }
+
+  @Test
+  public void test_calculateYearlySalary() {
+    person.setSalary(1200);
+    assertEquals(14400, person.calculateYearlySalary());
+  }
+
+  @Test
+  public void person_is_MEI() {
+    person.setAnotherCompanyOwner(false);
+    person.setPensioner(false);
+    person.setPublicServer(false);
+
+    assertTrue(person.isMEI());
+  }
+
+  @Test
+  public void person_is_not_MEI() {
+    person.setPensioner(true);
+    assertFalse(person.isMEI());
+  }
+}
