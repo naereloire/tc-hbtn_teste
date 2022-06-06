0de6f49cfcd6da0dd9e99c342157930895de8a15
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-06 19:12:31 -0300

    [junit II] rename pckage

--- 0x02/password /src/PersonTest.java
+++ 0x02/password /src/PersonTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class PessoaTest {
+public class PersonTest {
  Person person;
 
   @BeforeAll
