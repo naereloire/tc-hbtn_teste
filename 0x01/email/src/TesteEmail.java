c1bea16731cff5f64e88f48fa8f15db9e4c6464a
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-06 12:03:38 -0300

    [junit I] fix methods with test annotations

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,11 +1,14 @@
 import org.junit.Test;
 import testes.Pessoa;
+import static org.junit.Assert.*;
 
 /**
  * Pessoa Tester.
  *
  * @author <Authors name>
- * @since <pre>Jun 6, 2022</pre>
+ * @since
+ *     <pre>Jun 6, 2022</pre>
+ *
  * @version 1.0
  */
 public class TesteEmail {
@@ -13,26 +16,22 @@ public class TesteEmail {
     throw new IllegalStateException("Utility class");
   }
 
-  /**
-   *
-   * Method: emailValid(String email)
-   *
-   */
+  /** Method: emailValid(String email) */
   @Test
-  public static boolean testar_email_com_arroba() {
+  void testar_email_com_arroba() {
     String email = "email_teste@dominio.com.br";
-    return Pessoa.emailValid(email);
+    assertTrue(Pessoa.emailValid(email));
   }
 
   @Test
-  public static boolean testar_email_sem_arroba() {
+  void testar_email_sem_arroba() {
     String email = "email_testedominio.com.br";
-    return !Pessoa.emailValid(email);
+    assertTrue(Pessoa.emailValid(email));
   }
 
   @Test
-  public static boolean testar_email_mais_50_caracteres() {
+  void testar_email_mais_50_caracteres() {
     String email = "email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br";
-    return !Pessoa.emailValid(email);
+    assertEquals(false, Pessoa.emailValid(email));
   }
 }
